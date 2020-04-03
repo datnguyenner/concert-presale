14afed6a890838a7a9ab3c608ea74c85b37a4775
Dat Nguyen <datnguyen@Dats-Air.hsd1.ca.comcast.net>
2020-04-03 16:28:52 -0700

    Update signin and register components with backend service

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,26 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Register.css';
 
-const Register = ({ register }) => {
-    return (
-        <div className="register">
-            <h1>Register</h1>
-            <div>
-                <label htmlFor="email">Email<br/></label>
-                <input type="text" placeholder="Enter Email" name="email" required />
+class Register extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            password: ''
+        }
+    }
+
+    onchangeEmail= (e) => {
+        this.setState({ email: e.target.value });
+    }
+
+    onchangePassword = (e) => {
+        this.setState({ password: e.target.value });
+    }
+
+    onchangeName = (e) => {
+        this.setState({ name: e.target.value });
+    }
+
+    onSubmit = async () => {
+        const response = await fetch('http://localhost:3000/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify(this.state)
+        });
+
+        console.log(JSON.stringify(this.state));
+        if(response.status === 201){
+            const data = await response.json();
+            this.props.register('Home');
+        }
+    }
+
+    render() {
+        return (
+            <div className="register">
+                <h1>Register</h1>
+                <div>
+                    <label htmlFor="email">Email<br /></label>
+                    <input type="text" placeholder="Enter Email" onChange={this.onchangeEmail} name="email" required />
+                </div>
+                <div className="userName">
+                    <label htmlFor="uname">Username<br /></label>
+                    <input type="text" placeholder="Enter Username" onChange={this.onchangeName} name="uname" required />
+                </div>
+                <div className="password">
+                    <label htmlFor="psw">Password<br /></label>
+                    <input type="password" placeholder="Enter Password" onChange={this.onchangePassword} name="psw" required />
+                </div>
+                <button onClick={this.props.register.bind(this, 'SignIn')}>Cancel</button>
+                <button onClick={this.onSubmit}>Register</button>
             </div>
-            <div className="userName">
-                <label htmlFor="uname">Username<br/></label>
-                <input type="text" placeholder="Enter Username" name="uname" required />
-            </div>
-            <div className="password">
-                <label htmlFor="psw">Password<br/></label>
-                <input type="password" placeholder="Enter Password" name="psw" required />
-            </div>
-            <button onClick={register.bind(this, 'SignIn')}>Cancel</button>
-            <button onClick={register.bind(this, 'Home')}>Register</button>
-        </div>
-    );
+        );
+    }
 }
 
 export default Register;
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SignIn.css';
 
-const SignIn = ({ signIn, register }) => {
-    return (
-        <div className="signIn">
-            <h1>Sign in</h1>
-            <div className="userName">
-                <label htmlFor="uname">Username<br /></label>
-                <input type="text" placeholder="Enter Username" name="uname" required />
+class SignIn extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    onchangeUserName = (e) => {
+        this.setState({ email: e.target.value });
+    }
+
+    onchangePassword = (e) => {
+        this.setState({ password: e.target.value });
+    }
+
+    onSubmit = async () => {
+        const response = await fetch('http://localhost:3000/signin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify(this.state)
+        });
+
+        console.log(JSON.stringify(this.state));
+        if(response.status === 200){
+            const data = await response.json();
+            this.props.signIn('Home');
+        }
+    }
+
+    render() {
+        const { register } = this.props;
+        return (
+            <div className="signIn">
+                <h1>Sign in</h1>
+                <div className="userName">
+                    <label htmlFor="uname">Username<br /></label>
+                    <input type="text" placeholder="Enter Username" onChange={this.onchangeUserName} name="uname" required />
+                </div>
+                <div className="password">
+                    <label htmlFor="psw">Password<br /></label>
+                    <input type="password" placeholder="Enter Password" onChange={this.onchangePassword} name="psw" required />
+                </div>
+                <div>
+                    <button className="registerBtn" onClick={register.bind(this, 'Register')}>Register</button>
+                </div>
+                <button className="signInBtn" onClick={this.onSubmit}>Login</button>
             </div>
-            <div className="password">
-                <label htmlFor="psw">Password<br /></label>
-                <input type="password" placeholder="Enter Password" name="psw" required />
-            </div>
-            <div>
-                <button className="registerBtn" onClick={register.bind(this, 'Register')}>Register</button>
-            </div>
-            <button className="signInBtn" onClick={signIn.bind(this, 'Home')}>Login</button>
-        </div>
-    );
+        );
+    }
 }
 
 export default SignIn;
